333210b0f985505bb6ad8a4ca13701a971fd68bc
kesio20 <125152531+kesio20@users.noreply.github.com>
2023-02-25 16:08:48 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import this
+print('Olá Mundo')
+num1 = 10
+num2 = 5
+
+soma = num1 + num2
+subtracao = num1 - num2
+multiplicacao = num1 * num2
+divisao = num1 / num2
+exponenciacao = num1**num2
+
+print("soma:", soma)
+print("subtracao:", subtracao)
+print("multiplicacao:", multiplicacao)
+print("divisao:", divisao)
+print("exponenciação:", exponenciacao)
+
+num1 = 10
+num2 = 20
+num3 = 30
+
+media = (num1 + num2 + num3) / 3
+print("A media dos numeros é:", media)
+
